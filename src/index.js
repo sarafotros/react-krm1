6041fa41d3888a9110f414a479587a96bc5fb56e
sarafotros <sarafotros@gmail.com>
2020-04-10 14:33:32 +0100

    screens folder and componetets created

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import Home from "./screens/Home";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Home extends React.Component {
+  
+    render() { 
+        return (
+        <div>
+            <h2 style={{ margin:240, backgroundColor:'lightBlue'}}> Hi</h2>
+        </div>
+     );
+    }
+}
+ 
+export default Home;
