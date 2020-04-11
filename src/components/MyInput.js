a757416957f1c76c3c5d07fb62f5ac53fb13542b
sarafotros <sarafotros@gmail.com>
2020-04-11 20:24:16 +0100

    input forms validation

---
+++ src/components/MyInput.js
@@ -0,0 +1,27 @@
+import React from 'react'
+class MyInput extends React.Component {
+    state = { 
+        error: false
+    }
+    
+    render() { 
+        let regex = this.props.regex
+        let errorText =  this.props.errorText
+        return ( 
+            <div style={{ color: 'black', width: '100%', padding:15, display: 'flex', justifyContent:'center', alignItems: 'center',flexDirection: 'column' }}>
+                <input onBlur={(e) => {
+                    let value = e.target.value
+                    let emailregex = regex
+                    this.setState({
+                        error: !emailregex.test(value)
+                    })
+                }}
+                style={{ width: '80%', border: 'none', borderBottom: '1px solid #000' }} type='text' placeholder='input...'></input>
+                {this.state.error ? (<span style={{ color: 'red', fontSize: 12, marginTop: 5 }}>{errorText}</span>) : (null)}
+               
+            </div>
+         );
+    }
+}
+ 
+export default MyInput;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React from 'react';
 import styles from './styles';
 import Mybtn from '../components/Mybtn';
-import MyTime from '../components/MyTime'
-
+import MyTime from '../components/MyTime';
+import MyInput from '../components/MyInput';
 class Home extends React.Component {
-  
-    render() { 
-     return (
-        <div style={styles.body}>
-            <div style={styles.header}>
-                <div style={styles.right}>
-                    <p> React Developer</p>
-                </div>
-                <div style={styles.center}></div>
-                 <div style={styles.left}>
-                     <MyTime/>
-                </div>
-            </div>
-            <div style={styles.main}>
-                <div style={styles.form}>
-                    <Mybtn title="login" />
-                    <Mybtn title="signup" bg="#EA80FC" />
-                </div>
-            </div>
-            <div style={styles.footer}> hello</div>
-        </div>
-	 );
-    }
+	render() {
+		return (
+			<div style={styles.body}>
+				<div style={styles.header}>
+					<div style={styles.right}>
+						<p> React Developer</p>
+					</div>
+					<div style={styles.center}></div>
+					<div style={styles.left}>
+						<MyTime />
+					</div>
+				</div>
+				<div style={styles.main}>
+					<div style={styles.form}>
+                        <MyInput regex={/^[a-z0-9._]{3,}@[a-z]{2,}\.[a-z]{2,4}$/i} errorText='invalid email format' />
+                        <MyInput regex={/^09[0-9]{9}$/} errorText='invalid mobile number format' />
+						<Mybtn title="login" />
+						<Mybtn title="signup" bg="#EA80FC" />
+					</div>
+				</div>
+				<div style={styles.footer}> hello</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Home;
