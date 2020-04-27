cc542a4e373be01d081c759eeb2cf8b5a35fa42b
sarafotros <sarafotros@gmail.com>
2020-04-27 21:31:18 +0100

    increase decrease btn

--- src/components/myfunccmp.js
+++ src/components/myfunccmp.js
@@ -4,14 +4,40 @@ export default (props) => {
     const [count, setCount] = useState (0)
     
     return (
-        <div>
-            <MyText txt={count}/>
-        </div>
-    )
+			<div>
+				<MyText txt={count} />
+				<button
+					style={{
+						width: 90,
+						height: 35,
+						background: 'none',
+						borderBottom: '2px solid grey',
+					}}
+					onClick={() => {
+						setCount((count) => count + 1);
+					}}
+				>
+					Increase
+				</button>
+				<button
+					style={{
+						width: 90,
+						height: 35,
+						background: 'none',
+						borderBottom: '2px solid grey',
+					}}
+					onClick={() => {
+						setCount((count) => count - 1);
+					}}
+				>
+					Decrease
+				</button>
+			</div>
+		);
 }
 function MyText(props) {
     return (
-        <span>
+        <span style={{padding: 15, fontSize:20}}>
           {props.txt}
         </span>
     )
